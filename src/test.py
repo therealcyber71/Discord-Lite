0bd3f6d51d7c7ff3c2a115c91d1ac0f7099c42ba
Sachit <72592470+Sachit71@users.noreply.github.com>
2022-01-08 13:18:07 +0530

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,18 +1,18 @@
+
+import sys
+import os
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
 import os
 import sys
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 
-import ray
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtPrintSupport import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtWebEngineWidgets import QWebEnginePage, QWebEngineView
-from PyQt5.QtWidgets import *
-
-ray.init()
+from multiprocessing import Process
 
 
-@ray.remote
 def class_runner():
     class WebEnginePage(QWebEnginePage):
         def __init__(self, *args, **kwargs):
@@ -20,18 +20,12 @@ def class_runner():
             self.featurePermissionRequested.connect(self.onFeaturePermissionRequested)
 
         def onFeaturePermissionRequested(self, url, feature):
-            if feature in (
-                QWebEnginePage.MediaAudioCapture,
-                QWebEnginePage.MediaVideoCapture,
-                QWebEnginePage.MediaAudioVideoCapture,
-            ):
-                self.setFeaturePermission(
-                    url, feature, QWebEnginePage.PermissionGrantedByUser
-                )
+            if feature in (QWebEnginePage.MediaAudioCapture, 
+                QWebEnginePage.MediaVideoCapture, 
+                QWebEnginePage.MediaAudioVideoCapture):
+                self.setFeaturePermission(url, feature, QWebEnginePage.PermissionGrantedByUser)
             else:
-                self.setFeaturePermission(
-                    url, feature, QWebEnginePage.PermissionDeniedByUser
-                )
+                self.setFeaturePermission(url, feature, QWebEnginePage.PermissionDeniedByUser)
 
     app = QApplication(sys.argv)
     app.setApplicationName("Discord Lite")
@@ -41,13 +35,16 @@ def class_runner():
     view.load(QUrl("https://discord.com/app"))
     view.show()
     scriptDir = os.path.dirname(os.path.realpath(__file__))
-    app.setWindowIcon(QIcon(scriptDir + os.path.sep + "logo4.png"))
+    app.setWindowIcon(QIcon(scriptDir + os.path.sep + 'logo4.png'))
     app.exec_()
 
-
-@ray.remote
 def screen_shot():
     import scrotrem
-
-
-ray.get([class_runner.remote(), screen_shot.remote()])
+def runn():
+    p1 = Process(target=class_runner) # create a process object p1
+    p1.start()                   # starts the process p1
+    p2 = Process(target=screen_shot)
+    p2.start()
+if __name__ == '__main__':
+    #freeze_support()
+    runn()
