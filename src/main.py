49468c68ec861b059c6c50edca27026b89813bdc
Sachit <72592470+Sachit71@users.noreply.github.com>
2022-02-02 22:54:57 +0530

    Changed some imports
    Deleted extra spaces

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5.QtCore import *
+from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
